d97907e71eb85bb5a692017535db678c0642bbb3
BHAVYA DHIMAN <bhavyadhiman7@gmail.com>
2023-09-16 19:56:36 +0530

    add landing page components

---
+++ src/components/svgs/VectorRight.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const VectorRight = () => {
+
+    return (
+        <svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path id="Vector" d="M1.65234 9.29543L5.2864 5.66137C5.34019 5.60772 5.38286 5.54398 5.41198 5.47381C5.4411 5.40364 5.45608 5.32842 5.45608 5.25245C5.45608 5.17648 5.4411 5.10126 5.41198 5.03109C5.38286 4.96092 5.34019 4.89718 5.2864 4.84353L1.65234 1.20947" stroke="#F35815" strokeWidth="1.7327" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    );
+}
