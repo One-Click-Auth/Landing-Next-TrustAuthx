4a2fe9bfd7a1d30547f5c00c5d0fb5eeaea70399
Pranav Rajveer <rajveer23pr@gmail.com>
2023-11-12 02:30:52 +0530

    added sitemap and robots file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+return [
+    {
+    url: 'https://www.trustauthx.com',
+    lastModified: new Date(),
+    changeFrequency: 'monthly',//we can do yearly also depends
+    priority: 1.0,
+    },
+    {
+    url: 'https://www.trustauthx.com/systems',
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.6,
+    },
+    {
+    url: 'https://www.trustauthx.com/accessManagement',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.6,
+    },
+]
+}
