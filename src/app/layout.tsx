6bf2012f5c8a95497344db060ce0a6784f33bdd8
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-11-16 04:35:14 +0530

    hydrating error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Script from "next/script";
 import "../styles/globals.css";
-import type  {Metadata} from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -12,10 +12,11 @@ const inter = Inter({
   display: "swap",
 });
 
-export const metadata:Metadata = {
-  title:"TrustAuthX",
-  description: 'TrustAuthX is a Low Code Authentication as a Service Platform for developers/businesses worldwide.',
-}
+export const metadata: Metadata = {
+  title: "TrustAuthX",
+  description:
+    "TrustAuthX is a Low Code Authentication as a Service Platform for developers/businesses worldwide.",
+};
 
 export default function RootLayout({
   children,
@@ -26,26 +27,28 @@ export default function RootLayout({
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
         <title>{`${metadata.title}`}</title>
-        <meta name="description" content={`${metadata.description}`} />.
+        <meta name="description" content={`${metadata.description}`} />
         <link rel="icon" href="./favicon.ico" />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-7NXTY5KCKL"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7NXTY5KCKL"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-7NXTY5KCKL');
         `}
-      </Script>
+        </Script>
       </head>
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        <div className="flex min-h-screen flex-col ">
+          <Navbar />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
