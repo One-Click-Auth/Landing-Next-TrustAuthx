92421467c9889836ffdf502411af3a6667576ba6
Pranav Rajveer <rajveer23pr@gmail.com>
2023-11-15 10:18:07 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,14 @@
 import { MetadataRoute } from "next";
 
-export default function robots():MetadataRoute.Robots{
-    return{
-        rules:{
-        userAgent:"*",
-        allow:"/",
-        disallow:"/private/"
-        },
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: "/private/"
+            }
+        ],
         sitemap: 'https://www.trustauthx.com/sitemap.xml',
-    }
+    };
 }
