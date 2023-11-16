2541f93ed15bf32d2e41913b2acb21dd124b400c
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-11-16 16:52:13 +0530

    gradient uncomment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function LandingPage() {
     <>
       <LandingHero />
       <AiInCLI />
-      {/* <GradientPlatform /> */}
+      <GradientPlatform />
       {/*       {!isMobile ? <Timeline /> : null} */}
       <Authlite />
       <Showcase />
