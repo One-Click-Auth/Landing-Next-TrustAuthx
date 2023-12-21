f214bd68277adf55dc4cd6e2a0153676d7c36bb5
moonlightnexus <68702919+moonlightnexus@users.noreply.github.com>
2023-12-21 20:21:53 +0530

    Update page.tsx

--- src/app/ppo/page.tsx
+++ src/app/ppo/page.tsx
@@ -1,39 +1,39 @@
 const PrivacyPolicy = () => {
     return ( 
-        <main className="flex relative h-screen flex-col
-        items-center justify-between p-36 max-sm:p-14 bg-black text-white">
-<h1 className="text-center text-3xl">Privacy Policy for TrustAuthX</h1>
-
-Introduction
-
-At TrustAuthX, accessible from trustauthx.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by TrustAuthX and how we use it.
-
-Personal Data
-
-While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to:
-
-- Email address
-- First name and last name
-- Phone number
-- Address, State, Province, ZIP/Postal code, City
-
-Log Files
-
-TrustAuthX follows a standard procedure of using log files. These files log visitors when they visit websites. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks.
-
-Cookies and Web Beacons
-
-Like any other website, TrustAuthX uses cookies. These cookies are used to store information including visitors preferences, and the pages on the website that the visitor accessed or visited.
-
-Third Party Privacy Policies
-
-TrustAuthX Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information.
-
-Consent
-
-By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.
-    </main>
-);
+        <main className="flex h-screen flex-col items-center justify-between p-36 max-sm:p-14 bg-black text-white">
+            <h1 className="text-center text-3xl">Privacy Policy for TrustAuthX</h1>
+            <section>
+                <h2>Introduction</h2>
+                <p>At TrustAuthX, accessible from trustauthx.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by TrustAuthX and how we use it.</p>
+            </section>
+            <section>
+                <h2>Personal Data</h2>
+                <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to:</p>
+                <ul>
+                    <li>Email address</li>
+                    <li>First name and last name</li>
+                    <li>Phone number</li>
+                    <li>Address, State, Province, ZIP/Postal code, City</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Log Files</h2>
+                <p>TrustAuthX follows a standard procedure of using log files. These files log visitors when they visit websites. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks.</p>
+            </section>
+            <section>
+                <h2>Cookies and Web Beacons</h2>
+                <p>Like any other website, TrustAuthX uses cookies. These cookies are used to store information including visitors preferences, and the pages on the website that the visitor accessed or visited.</p>
+            </section>
+            <section>
+                <h2>Third Party Privacy Policies</h2>
+                <p>TrustAuthX Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information.</p>
+            </section>
+            <section>
+                <h2>Consent</h2>
+                <p>By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>
+            </section>
+        </main>
+    );
 }
 
 export default PrivacyPolicy;
