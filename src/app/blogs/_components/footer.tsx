7e6c6bd5e7cd87376255011273f42a3ab86715ad
Pranav Rajveer <rajveer23pr@gmail.com>
2023-12-24 00:53:30 +0530

    Update footer.tsx

--- src/app/blogs/_components/footer.tsx
+++ src/app/blogs/_components/footer.tsx
@@ -5,7 +5,7 @@ export const FooterPage = () => {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className="flex items-center">
-                  <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
+{/*                   <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" /> */}
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TrustAuthX</span>
               </a>
           </div>
